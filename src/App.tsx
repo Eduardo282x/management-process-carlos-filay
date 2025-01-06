45feca733ae667fa79251783a506fc4d99242c86
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-01-06 11:46:22 -0400

    remove authLayout, add routes, complete login, add snackbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login/Login";
-import AuthLayout from "./layout/AuthLayout";
+import { Layout } from "./layout/Layout";
+import { Home } from "./pages/home/Home";
+import { Inscription } from "./pages/inscription/Inscription";
+import { Grades } from "./pages/grades/Grades";
+import { Payments } from "./pages/payments/Payments";
+import { Studies } from "./pages/studies/Studies";
+import { Users } from "./pages/users/Users";
 
 function App() {
 
   return (
-    
+
     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
 
-          <Routes>
-            {/* Login */}
-            <Route element={<AuthLayout />}>
-              <Route path='/auth/login' element={<Login />} />
-            </Route>
-          </Routes>
+        <Route element={<Layout />}>
+          <Route path='/inicio' element={<Home />} />
+          <Route path='/inscripcion' element={<Inscription />} />
+          <Route path='/grados' element={<Grades />} />
+          <Route path='/pagos' element={<Payments />} />
+          <Route path='/metodos-de-pago' element={<Payments />} />
+          <Route path='/estudiantes' element={<Studies />} />
+          <Route path='/usuarios' element={<Users />} />
+        </Route>
+      </Routes>
 
-      </BrowserRouter>
-
+    </BrowserRouter>
   );
 
 }
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,100 +1,75 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { LoginSchema, UserLoginForm } from "../../types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ScreenLoader } from "../../components/loaders/ScreenLoader";
 import ErrorMessage from "../../components/ErrorMessage";
 import Logo from "../../components/Logo";
-
+import { defaultValues, LoginSchema, UserLoginForm } from "./login.data";
+import { postDataApi } from "../../backend/basicAPI";
+import { BaseResponse, BaseResponseLogin } from "../../interfaces/base.interface";
+import { useNavigate } from "react-router-dom";
+import { SnackbarComponent } from "../../components/SnackbarComponent";
 
 export const Login = () => {
-
+    const navigate = useNavigate();    
+    const [open, setOpen] = useState(false);
+    const [responseApi, setResponseApi] = useState<BaseResponseLogin>({ message: '', success: false, userData: '' });
     const [loader, setLoader] = useState<boolean>(false);
-
-    const defaultValues: UserLoginForm = {
-        username: '',
-        password: ''
-    }
-
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm<UserLoginForm>({
         defaultValues,
         resolver: zodResolver(LoginSchema)
     });
 
-    function successfulLogin(formData: UserLoginForm) {
+    async function successfulLogin(formData: UserLoginForm) {
         setLoader(true);
-        if (formData.username === 'admin' && formData.password === 'admin') {
-            reset();
-            console.log('Login successful');
-        } else {
-            reset();
-            console.log('User Not Found');
-        }
-
-        setTimeout(() => {
+        await postDataApi('/auth', formData).then((response: BaseResponseLogin | BaseResponse) => {
+            const returnApi = response as BaseResponseLogin;
+            setResponseApi(response as BaseResponseLogin);
+            setOpen(true);
+            if (response.success) {
+                localStorage.setItem('userData', JSON.stringify(returnApi.userData));
+                setTimeout(() => {
+                    navigate('/inicio')
+                }, 1500);
+            }
             setLoader(false);
-        }, 1000);
+        });
     }
 
     return (
 
-        <div className="bg-white rounded-lg px-4 py-6">
-            
-            {loader && (
-                <ScreenLoader></ScreenLoader>
-            )}
-
-            <div className="flex justify-center items-center">
-                
-                <Logo widthLogo="w-35" heightLogo="h-35" />
-
-            </div>
-
-            <form onSubmit={handleSubmit(successfulLogin)} className="space-y-6 p-4" noValidate>
-                
-                <div className="flex flex-col gap-2">
-                    <label className="font-normal text-lg">Nombre de Usuario</label>
-                    <input type="username" placeholder="Arcenio24Y"
-                        className="w-full p-3 border-gray-300 border outline-none rounded-lg"
-                        {...register("username")}
-                    />
-                    {errors.username && (<ErrorMessage>{errors.username.message}</ErrorMessage>)}
+        <div className="bg-blue-600 h-screen w-screen flex items-center justify-center">
+            <div className="bg-white rounded-lg px-4 py-6 w-[25rem]">
+                {loader && (
+                    <ScreenLoader></ScreenLoader>
+                )}
+                <div className="flex justify-center items-center">
+                    <Logo widthLogo="w-35" heightLogo="h-35" />
                 </div>
-
-                <div className="flex flex-col gap-2">
-                    <label className="font-normal text-lg">Contraseña</label>
-                    <input type="password" placeholder="********"
-                        className="w-full p-3  border-gray-300 border outline-none rounded-lg"
-                        {...register("password")}
-                    />
-                    {errors.password && (<ErrorMessage>{errors.password.message}</ErrorMessage>)}
-                </div>
-
-                <div className="flex items-center justify-between">
-
-                    <div className="flex items-start">
-                        
-                        <div className="flex items-center h-5">
-                            <input type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50"/>
-                        </div>
-                        <div className="ml-3 text-sm">
-                            <label className="text-gray-500">Mantener sesión activa</label>
-                        </div>
-
+                <form onSubmit={handleSubmit(successfulLogin)} className="space-y-6 p-4" noValidate>
+                    <div className="flex flex-col gap-2">
+                        <label className="font-normal text-lg">Nombre de Usuario</label>
+                        <input type="username" placeholder="Usuario"
+                            className="w-full p-3 border-gray-300 border outline-none rounded-lg"
+                            {...register("username")}
+                        />
+                        {errors.username && (<ErrorMessage>{errors.username.message}</ErrorMessage>)}
                     </div>
 
-                    <a href="#" className="text-sm font-medium hover:underline">¿Olvidaste tu contraseña?</a>
+                    <div className="flex flex-col gap-2">
+                        <label className="font-normal text-lg">Contraseña</label>
+                        <input type="password" placeholder="********"
+                            className="w-full p-3  border-gray-300 border outline-none rounded-lg"
+                            {...register("password")}
+                        />
+                        {errors.password && (<ErrorMessage>{errors.password.message}</ErrorMessage>)}
+                    </div>
 
-                </div>
+                    <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Iniciar Sesión</button>
 
-                <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Iniciar Sesión</button>
-
-                <p className="text-sm font-light text-gray-500 text-center">
-                    ¿No tienes una cuenta aún? <a href="#" className="font-medium hover:underline text-gray-600">Regístrate ahora</a>
-                </p>
-
-            </form>
-
+                </form>
+            </div>
+            <SnackbarComponent baseResponse={responseApi} open={open} setOpen={setOpen}></SnackbarComponent>
         </div>
     );
 
